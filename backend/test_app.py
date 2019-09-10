03560dbd9ae3f9af7ca8ce17ca1c32c90b1b25ef
Roy Chiu <rychiu13@gmail.com>
2019-09-10 17:44:35 -0500

    fix backend tests

--- backend/test_app.py
+++ backend/test_app.py
@@ -14,28 +14,28 @@ def test_mirror(client):
     assert res.json["result"]["name"] == "Tim"
 
 
-def test_get_users(client):
-    res = client.get("/users")
+def test_get_contacts(client):
+    res = client.get("/contacts")
     assert res.status_code == 200
 
-    res_users = res.json["result"]["users"]
+    res_users = res.json["result"]["contacts"]
     assert len(res_users) == 4
-    assert res_users[0]["name"] == "Aria"
+    assert res_users[0]["name"] == "Angad"
 
 
-def tests_get_users_with_team(client):
-    res = client.get("/users?team=LWB")
+def tests_get_contacts_with_hobby(client):
+    res = client.get("/contacts?hobby=weeb")
     assert res.status_code == 200
 
-    res_users = res.json["result"]["users"]
-    assert len(res_users) == 2
-    assert res_users[1]["name"] == "Tim"
+    res_users = res.json["result"]["contacts"]
+    assert len(res_users) == 1
+    assert res_users[1]["name"] == "Roy"
 
 
-def test_get_user_id(client):
-    res = client.get("/users/1")
+def test_get_contacts_id(client):
+    res = client.get("/contacts/1")
     assert res.status_code == 200
 
-    res_user = res.json["result"]["user"]
-    assert res_user["name"] == "Aria"
-    assert res_user["age"] == 19
+    res_user = res.json["result"]["contacts"]
+    assert res_user["name"] == "Angad"
+    assert res_user["hobby"] == "dirty-ing"
